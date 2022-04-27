3f51151bc0189ba711cdfde27d77edfa13570321
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-04-27 20:32:38 +0300

    final commit

--- password.py
+++ password.py
@@ -109,6 +109,7 @@ class Credentials():
 
         """
         return cls.credentials_list
+        
 
     def generatePassword(stringLength=8):
         """Generate a random password string of letters and digits and special characters"""
