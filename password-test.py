59c191a8cb21b3bab6cd8e33c915b5006d88e8cb
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-04-27 20:27:07 +0300

    dummy user credentials set

--- password-test.py
+++ password-test.py
@@ -7,12 +7,12 @@ class TestClass(unittest.TestCase):
     """
     A Test class that defines test cases for the User class.
     """
-    
+
     def setUp(self):
         """
         Method that runs before each individual test methods run.
         """
-        self.new_user = User('MarvinKimathi','XyZ3thf1')
+        self.new_user = User('MarvinKimathi', 'XyZ3thf1')
 
 
     def test_init(self):
