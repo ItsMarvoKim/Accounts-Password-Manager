fe2f4683f76a9c02eac384c9d813715431717407
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-04-27 19:51:04 +0300

    identation corrected

--- password.py
+++ password.py
@@ -2,11 +2,13 @@ import random
 import string
 
 class User:
+
     """
     Create User class that generates new instances of a user.
 
     """
     user_list = []
+
 
     def __init__(self, username, password):
         """
@@ -14,6 +16,7 @@ class User:
         """
         self.username = username
         self.password = password
+
 
     def save_user(self):
         """
